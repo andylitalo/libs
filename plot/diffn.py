ff0cd1bca4ace5c798fbb7175c98e629cc801d6c
andylitalo <aylitalo@caltech.edu>
2020-12-01 06:27:20 -0800

    DIsplays 6 decimal places of t in sheath

--- plot/diffn.py
+++ plot/diffn.py
@@ -77,8 +77,8 @@ def sheath(t, r_arr, c, R_i, R_o, v, c_s, n_plot, ax=None, t_fs=18,
 
     # plots CO2 concentration profile at evenly spaced time points
     for i in range(0, n_steps, skip):
-        ax.plot(r_arr*m_2_um, c[i], label='t = {0:d} ms, d = {1:d} mm' \
-                .format(int(t[i]*s_2_ms), int(v*t[i]*m_2_mm)))
+        ax.plot(r_arr*m_2_um, c[i], label='t = {0:.6f} ms, d = {1:d} mm' \
+                .format(t[i]*s_2_ms, int(v*t[i]*m_2_mm)))
     # plots vertical dashed line marking inner radius
     ax.plot([R_i*m_2_um, R_i*m_2_um], y_lim, 'k--', label=r'$R_i$' + \
                 ' = {0:d} '.format(int(R_i*m_2_um)) + r'$\mu$m')
