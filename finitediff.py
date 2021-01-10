3da5c90a42f2c1928bf83f4f3174785e3c4212c6
andylitalo <aylitalo@caltech.edu>
2021-01-09 16:03:26 -0800

    Fixed bug in finitediff.py (dydx_fwd_2nd())

--- finitediff.py
+++ finitediff.py
@@ -40,4 +40,4 @@ def dydx_fwd_2nd(y0, y1, y2, dx):
     """
     Computes 2nd-order forward difference of dy/dx with a Taylor scheme.
     """
-    return = (-3*y0 + 4*y1 - y2)/(2*dx)
+    return (-3*y0 + 4*y1 - y2)/(2*dx)
